e61ec28b8045773f99b85fd0b87749aa271adbc3
Viktor Belkin <Viktor.Belkin@nexign.com>
2026-04-05 13:43:29 +0200

    Fix API controller tests: correct route helpers, HTTP methods, and add Owner.predefined_owner

--- test/controllers/api/bookings_controller_test.rb
+++ test/controllers/api/bookings_controller_test.rb
@@ -2,22 +2,26 @@ require "test_helper"
 
 class Api::BookingsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get api_bookings_index_url
+    get api_bookings_url
     assert_response :success
   end
 
-  test "should get create" do
-    get api_bookings_create_url
+  test "should create booking" do
+    assert_difference("Booking.count", 1) do
+      post api_bookings_url, params: { booking: { event_id: events(:one).id, slot: "2026-04-06 10:00" } }
+    end
+    assert_response :created
+  end
+
+  test "should show booking" do
+    get api_booking_url(id: bookings(:one).id)
     assert_response :success
   end
 
-  test "should get show" do
-    get api_bookings_show_url
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get api_bookings_destroy_url
-    assert_response :success
+  test "should destroy booking" do
+    assert_difference("Booking.count", -1) do
+      delete api_booking_url(id: bookings(:one).id)
+    end
+    assert_response :no_content
   end
 end
--- test/controllers/api/events_controller_test.rb
+++ test/controllers/api/events_controller_test.rb
@@ -2,22 +2,26 @@ require "test_helper"
 
 class Api::EventsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get api_events_index_url
+    get api_events_url
     assert_response :success
   end
 
-  test "should get show" do
-    get api_events_show_url
+  test "should show event" do
+    get api_event_url(id: events(:one).id)
     assert_response :success
   end
 
-  test "should get create" do
-    get api_events_create_url
-    assert_response :success
+  test "should create event" do
+    assert_difference("Event.count", 1) do
+      post api_events_url, params: { event: { name: "Test Event", description: "A test", duration: 30 } }
+    end
+    assert_response :created
   end
 
-  test "should get destroy" do
-    get api_events_destroy_url
-    assert_response :success
+  test "should destroy event" do
+    assert_difference("Event.count", -1) do
+      delete api_event_url(id: events(:one).id)
+    end
+    assert_response :no_content
   end
 end
